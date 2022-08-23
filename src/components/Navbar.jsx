61155f369b487b683a9b5f961c4c3fb91625c9a4
Joivy11 <buituanhuu925@gmail.com>
2022-08-24 01:45:33 +0700

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width:100%;
+    height:60px;
+    box-sizing:border-box;
+`;
+
+const Navbar = () => {
+    return (
+        <Container>
+            Navbar
+        </Container>
+    )
+}
+
+export default Navbar;
