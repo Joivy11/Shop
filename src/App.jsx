3ee8c891a6ba584ab42cd5ddb0c25ad58439ac90
Joivy11 <buituanhuu925@gmail.com>
2022-09-03 13:44:52 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route, 
+  Navigate
+} from "react-router-dom";
 
 const App = () => {
+  const user = true;
+
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">
+          {user ? <Navigate to="/" /> : <Login />}
+        </Route>
+        <Route path="/register">
+          {user ? <Navigate to="/" /> : <Register />}
+        </Route>
+      </Routes>
+    </Router>
   )
 };
 
