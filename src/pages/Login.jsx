ec8afec7be568823ea3a3cc5ddd159271ecd55fd
Joivy11 <buituanhuu925@gmail.com>
2022-09-06 22:53:34 +0700

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-
+import { login } from '../redux/apiCalls';
 
 const Container =styled.div`
     width: 100vw;
@@ -61,15 +62,30 @@ const Link = styled.a`
     cursor: pointer;
 `;
 
+const Error = styled.span`
+    color: red;
+`;
+
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const { isFetching, error } = useSelector((state) => state.user);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        login(dispatch, { username, password });
+    };
+
     return (
         <Container>
             <Wrapper>
                 <Title>SIGN IN</Title>
                 <Form>
-                    <Input placeholder='Username' />
-                    <Input placeholder='Password' />
-                    <Button>LOGIN</Button>
+                    <Input placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
+                    <Input placeholder='Password' type="password" onChange={(e) => setPassword(e.target.value)} />
+                    <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
+                    {error && <Error>Something went wrong...</Error>}
                     <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
                     <Link>CREATE A NEW ACCOUNT</Link>
                 </Form>
